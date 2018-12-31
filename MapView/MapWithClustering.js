aa26284c57c049f38c20d5af7f10d96455be79d7
bernard <bernard.kllogjri@softlab.al>
2018-12-31 13:10:09 +0100

    added support for onRegionChange

--- MapView/MapWithClustering.js
+++ MapView/MapWithClustering.js
@@ -54,7 +54,7 @@ export default class MapWithClustering extends Component {
         this.calculateClustersForMap(region);
       }
     }
-    this.props.onRegionChangeComplete(region)
+    this.props.onRegionChangeComplete(region);
   };
 
   createMarkersOnMap = () => {
